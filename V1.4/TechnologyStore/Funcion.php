18685e0093925c98c62e490c3e68cb90ca206583
andru9365 <andru9365@outlook.com>
2017-06-05 13:09:35 -0500

    V1.4
    Cambios en Pantalla principal y en estructura de sesión

---
+++ V1.4/TechnologyStore/Funcion.php
@@ -0,0 +1,53 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <?php
+            include 'conexion.php';
+
+            if (isset($_POST['reg'])) {
+                $cedula = $_POST['cedu'];
+                $nombre = $_POST['nomb'];
+                $apellido = $_POST['apel'];
+                $usuario = $_POST['user'];
+                $contra = $_POST['cont'];
+                $correo = $_POST['corr'];
+                $direccion = $_POST['dire'];
+                $ciudad = $_POST['ciud'];
+                $genero = $_POST['gene'];
+                $fecha = $_POST['fech'];
+//                $enciptar= sha1($contra);
+
+
+                $sql = "INSERT INTO usuario(cedulaU, nombre, apellido, usuario, password, correo, direccion, ciudad, genero, F_nacimiento) VALUES('$cedula','$nombre','$apellido','$usuario','$contra','$correo','$direccion','$ciudad','$genero','$fecha')";
+                $res = mysql_query($sql, $conexion) or die(mysql_error());
+
+                if (!$res) {
+                    echo 'Error al registrarse';
+                } else {
+//                       require 'index.php#LoginModal';
+
+                
+                    ?>
+ 
+
+                    <script>
+                        var msg = "El usuario a sido registrado"
+                        alert(msg);
+                      
+                         window.location="index.php";
+
+                    </script>
+
+
+        <?php
+    }
+}
+?>
+
+
+
+        </body>
+    </html>
---
+++ V1.4/TechnologyStore/validar.php
@@ -0,0 +1,30 @@
+<?php
+	session_start(); 
+?>
+<?php
+			include 'conexion.php';
+			if(isset($_POST['ini'])){
+				$usuario = $_POST['user'];
+				$pw = $_POST['pass'];
+				$log = mysql_query("SELECT * FROM usuario WHERE usuario='$usuario' AND password='$pw'");
+				if (mysql_num_rows($log)>0) {
+					$row = mysql_fetch_array($log);
+
+                                     
+                                        
+                                        echo '<script> alert("Bienvenido");</script>';
+					echo '<script> window.location="perfil.php"; </script>';
+                                         
+                                        
+				}
+				else{
+					echo '<script> alert("Usuario o contraseña incorrectos.");</script>';
+						echo '<script> window.location="#LoginModal"; </script>';
+                                      
+                                    
+               
+                                        
+				}
+
+			}
+		?>	
